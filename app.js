c64acc2c3bc67d9cc37f3e2f876cd30ce6214141
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 02:11:42 +0200

    server starts

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('working'); 
+const http = require('http')
+
+const server = http.createServer((req,res)=>{})
+
+server.listen(3000, ()=>{
+    console.log('server started on port 3000')
+})
