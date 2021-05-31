ee3674a06718bede3019eca7567e4a1c59b2ef76
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 12:41:10 +0200

    fechea de base de datos

--- Routes/Product.rout.js
+++ Routes/Product.rout.js
@@ -3,24 +3,37 @@ const router = express.Router();
 
 const Product = require('../Models/Product.model');
 
-router.get('/', (req, res,next) =>{
-    next(new Error("canot get list of products"));
-    res.send('getting a list of all products...'); 
+router.get('/', async (req, res,next) =>{
+   try{
+       const resoults = await Product.find();
+       res.send(resoults)
+
+   }catch(error){
+       console.log(error.message);
+   }
 });
 
-router.post('/', (req,res,next)=>{
-    console.log(req.body);
-    const product = new Product({
-        name: req.body.name,
-        price: req.body.price
-    })
-    product.save().then(resoult =>{
-        console.log(resoult);
+router.post('/', async (req,res,next)=>{
+    
+    try{
+        const product = new Product(req.body);
+        const resoult = await product.save();
         res.send(resoult);
-    }).catch(err=>{
-        console.log(err.message);
-    })
-    res.send();
+    }catch(error){
+        console.log(error.message);
+    }
+    
+    //const product = new Product({
+    //    name: req.body.name,
+    //    price: req.body.price
+    //});
+    //product.save().then(resoult =>{
+    //    console.log(resoult);
+    //    res.send(resoult);
+    //}).catch(err=>{
+    //    console.log(err.message);
+    //});
+    //res.send();
 });
 router.get('/:id', (req, res, next)=>{
     res.send('getting a single product');
