b220b3083a39f2f3998fbf253e8001c723f8c500
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 03:03:37 +0200

    errors handels express way

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const ProductsRoute = require('./Routes/Product.rout');
 app.use('/products', ProductsRoute);
 
 app.use((req,res,next)=>{
-    res.status(404)
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+
+//error handler
+app.use((err, res, re1, next)=>{
+    res.status(err.status || 500)
     res.send({
-        error: 'Not found'
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
     });
-})
+});
 
 app.listen(3000,()=>{
     console.log('Server started on port 3000...');
