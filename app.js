8a3f9c78693ff6ce376b6f3defbaa1dc15f65493
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 02:54:55 +0200

    404 managed

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 const ProductsRoute = require('./Routes/Product.rout');
 app.use('/products', ProductsRoute);
 
-
+app.use((req,res,next)=>{
+    res.status(404)
+    res.send({
+        error: 'Not found'
+    });
+})
 
 app.listen(3000,()=>{
     console.log('Server started on port 3000...');
