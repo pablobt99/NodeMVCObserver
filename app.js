05f1d1aec513151e8297394f181c9d9f175ee69f
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 02:15:44 +0200

    routs

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http')
 
-const server = http.createServer((req,res)=>{})
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write('ur on homepage');
+        res.end();
+    }else if(req.url=='/another'){
+        res.write('im another route');
+        res.end();
+    }
+    res.write("i am listening");
+    res.end();
+})
 
 server.listen(3000, ()=>{
     console.log('server started on port 3000')
