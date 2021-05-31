467beabdca1123eaafa1ff4c459f0b320bf3fd0b
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2021-05-31 12:10:47 +0200

    database on cloud writes and reads gg

--- Routes/Product.rout.js
+++ Routes/Product.rout.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const router = express.Router();
 
+const Product = require('../Models/Product.model');
+
 router.get('/', (req, res,next) =>{
     next(new Error("canot get list of products"));
     res.send('getting a list of all products...'); 
 });
 
 router.post('/', (req,res,next)=>{
-    res.send('product created');
+    console.log(req.body);
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price
+    })
+    product.save().then(resoult =>{
+        console.log(resoult);
+        res.send(resoult);
+    }).catch(err=>{
+        console.log(err.message);
+    })
+    res.send();
 });
 router.get('/:id', (req, res, next)=>{
     res.send('getting a single product');
